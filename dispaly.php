700c18836fa84c327f8d5d6fecd9a8a7d35bed4c
ShathaFA <shosho6500@gmail.com>
2024-04-24 17:30:51 +0300

    new

---
+++ dispaly.php
@@ -0,0 +1,28 @@
+<?php
+include_once('heade.php');
+$key = 'qkwjdiw239&&jdafweihbrhnan&"%Sggdnawhd4njshjwuuO';
+function encryptthis($data, $key) {
+$encryption_key = base64_decode($key);
+$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+$encrypted = openssl_encrypt($data, 'aes-256-cbc', $encryption_key, 0, $iv);
+return base64_encode($encrypted . '::' . $iv);
+}
+function decryptthis($data, $key) {
+$encryption_key = base64_decode($key);
+list($encrypted_data, $iv) = array_pad(explode('::', base64_decode($data), 2),2,null);
+return openssl_decrypt($encrypted_data, 'aes-256-cbc', $encryption_key, 0, $iv);}
+
+$result = $con->query("SELECT * FROM people") ;
+while ($row = $result->fetch_assoc()) {
+$name= $row['name'];
+$email= $row['email'];
+$reg_date= $row['reg_date'];
+echo '<p>Name: '.$name.'</p>';
+echo '<p>Email: '.$email.'</p>';
+echo '<p>reg_date: '.$reg_date.'</p>';
+
+}
+$con->close();
+?>
+
+
---
+++ heade.php
@@ -0,0 +1,11 @@
+<?php
+date_default_timezone_set('America/New_York');
+include_once('config.php');
+?>
+<html>
+<head>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css" >
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+</head>
+<body>
+<div class="container">
